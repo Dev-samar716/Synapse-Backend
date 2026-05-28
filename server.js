4fd5665eb8681a98e66fdd97db91bd868b4f2f58
Samar Kasaudhan <samardevworks@gmail.com>
2026-05-28 18:44:39 +0545

    Deleted irrelevant files from the codebase

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-import pool from "./src/config/db.js";
 import app from "./src/app.js";
 import chatRouter from "./src/routers/features/chat/chatRouters.js";
-
-
-// DB connection
-try {
-    await pool.query('SELECT 1');
-
-    console.log("Database connection successful!");
-} catch(error) {
-    console.log(error);
-    process.exit(1);
-}
 
 // router middlewares
 app.use('/chat', chatRouter)
