1289ae442e9eb3a9ec613d6513e66de247c1bf37
Samar Kasaudhan <samardevworks@gmail.com>
2026-06-10 13:32:17 +0545

    Programmed the createConversation function to initialize a conversation record in DB

---
+++ src/functions/createConversation.js
@@ -0,0 +1,15 @@
+import pool from "../config/db.js"
+
+const createConversation = async(title, user_id) => {
+     try {
+        const insert_query = "INSERT INTO conversations (created_at, user_id, title) VALUES ($1, $2, $3) RETURNING *"
+        const createdConversation = await pool.query(insert_query, [Date.now(), user_id, title]);
+
+        return createdConversation
+     } catch(error) {
+        console.log(error); 
+        return null
+     }
+}
+
+export default createConversation;
